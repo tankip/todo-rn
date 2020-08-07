5db5602762aa13efb0cc79186fb282d4165e6094
Rodgers Tanui <rtankip@gmail.com>
2020-08-07 13:54:28 +0300

    Created a todo app in react native

--- App.js
+++ App.js
@@ -1,21 +1,66 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React, { useState } from "react";
+import { StyleSheet, View, FlatList, Alert, TouchableWithoutFeedback, Keyboard } from "react-native";
+import Header from "./components/Header";
+import TodoItem from "./components/TodoItem";
+import AddTodo from "./components/AddTodo";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+const App = () => {
+    const [todos, setTodos] = useState([
+        { key: "1", text: "Eat" },
+        { key: "2", text: "Code" },
+        { key: "3", text: "Sleep" },
+        { key: "4", text: "Repeat" },
+        { key: "5", text: "Netflix" },
+    ]);
+
+    const pressHandler = (key) => {
+        setTodos((prev) => {
+            return prev.filter((todo) => todo.key != key);
+        });
+    };
+
+    const submitHandler = (text) => {
+        if (text.length > 2) {
+            Keyboard.dismiss();
+            setTodos((prev) => {
+                return [{ text, key: Math.random().toString() }, ...prev];
+            });
+        } else {
+            Alert.alert("Oops!", "Todos must be over 2 characters length", [{ text: "Understood", onPress: () => console.log("alert closed") }]);
+        }
+    };
+    return (
+        <TouchableWithoutFeedback
+            onPress={() => {
+                Keyboard.dismiss();
+            }}>
+            <View style={styles.container}>
+                <Header />
+                <View style={styles.content}>
+                    <AddTodo submitHandler={submitHandler} />
+                    <View style={styles.list}>
+                        <FlatList data={todos} renderItem={({ item }) => <TodoItem item={item} pressHandler={pressHandler} />} />
+                    </View>
+                </View>
+                <StatusBar style='auto' />
+            </View>
+        </TouchableWithoutFeedback>
+    );
+};
+export default App;
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: "#fff",
+    },
+    content: {
+        flex: 1,
+        padding: 40,
+    },
+    list: {
+        flex: 1,
+        marginTop: 20,
+    },
 });
---
+++ components/AddTodo.js
@@ -0,0 +1,27 @@
+import React, { useState } from "react";
+import { StyleSheet, Text, View, TextInput, Button } from "react-native";
+
+const AddTodo = ({ submitHandler }) => {
+    const [text, setText] = useState("");
+    const changeHandler = (val) => {
+        setText(val);
+    };
+    return (
+        <View>
+            <TextInput style={styles.input} placeholder='New todo...' onChangeText={changeHandler} />
+            <Button onPress={() => submitHandler(text)} title='Add Todo' color='coral' />
+        </View>
+    );
+};
+
+export default AddTodo;
+
+const styles = StyleSheet.create({
+    input: {
+        marginBottom: 10,
+        paddingHorizontal: 8,
+        paddingVertical: 6,
+        borderBottomWidth: 1,
+        borderBottomColor: "#ddd",
+    },
+});
